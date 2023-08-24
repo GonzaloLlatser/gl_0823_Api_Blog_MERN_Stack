d7d90a9e0b429ab4617090085721a9cbaf8c1546
GonzaloLlatser <gonllat@gmail.com>
2023-08-24 12:07:25 +0200

    feat: Crea base de datos para el proyecto, conecta a la base por medio de NodeJS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log ("Hola que tal, como estas");
+const { conexion } = require("./basedatos/conexion");
+
+//Inicializar app
+console.log("Hola que tal, como estas");
+
+//Conectar a la base de datos
+conexion();
+
+
