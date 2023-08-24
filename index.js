fb48e554fd695b253724df3e0445a311a1ddca4f
GonzaloLlatser <gonllat@gmail.com>
2023-08-24 12:22:49 +0200

    feat: Asigna servidor de Node.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { conexion } = require("./basedatos/conexion");
+const express = require("express");
+const cors = require("cors")
 
 //Inicializar app
 console.log("Hola que tal, como estas");
@@ -6,4 +8,20 @@ console.log("Hola que tal, como estas");
 //Conectar a la base de datos
 conexion();
 
+//Crear servidor de Node
+const app = express();
+const puerto = 3900;
 
+
+//Configurar cors
+app.use(cors());
+
+//Convertir body a objeto js
+app.use(express.json());
+
+//Crea rutas
+
+//Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor corriendo en el puerto" + puerto);
+});
